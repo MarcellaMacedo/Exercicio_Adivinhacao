cec2f901ea3b06fe77cd005e1dc6866880837cf7
Marcella_Mac84 <macedomarcella84@gmail.com>
2020-09-08 20:55:52 -0300

    Add files via upload

---
+++ testes5.py
@@ -0,0 +1,23 @@
+"""""
+import random
+
+aleatorio = random.randrange(10)
+print(aleatorio)
+
+"""
+
+"""""
+import random
+
+sorteado = random.randrange(0,4)
+
+print(sorteado)
+
+if sorteado == 1:
+    print( "Paulo")
+elif sorteado == 2:
+    print("Juliana")
+else:
+    print("Tamires")
+"""
+
